66c15d84a665d9c0447bcb9e4f1efb54efff4a5b
a <a@a.com>
2025-02-13 15:44:04 +0800

    a

--- src/operations/mint_ft.rs
+++ src/operations/mint_ft.rs
@@ -7,6 +7,7 @@ use bitcoin::opcodes::all::OP_RETURN;
 use bitcoin::locktime::absolute::LockTime;
 use serde::{Serialize, Deserialize};
 use std::error::Error as StdError;
+use web_sys::console;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct MintData {
@@ -21,8 +22,12 @@ pub async fn mint_ft<W: WalletProvider>(
     config: Arc20Config,
     mining_options: Option<MiningOptions>,
 ) -> Result<AtomicalsTx> {
+    console::log_1(&"Starting mint_ft operation...".into());
+
     // Get wallet address
+    console::log_1(&"Getting wallet address...".into());
     let address = wallet.get_address().await.map_err(|e| Error::WalletError(e.to_string()))?;
+    console::log_1(&format!("Got wallet address: {}", address).into());
     
     // Create mint data
     let mint_data = MintData {
@@ -31,9 +36,11 @@ pub async fn mint_ft<W: WalletProvider>(
         tick: config.tick.clone(),
         amt: config.mint_amount.0,
     };
+    console::log_1(&format!("Created mint data: {:?}", mint_data).into());
     
     // Create data payload
     let payload = serde_json::to_vec(&mint_data).map_err(|e| Error::from(e))?;
+    console::log_1(&format!("Created payload of size: {}", payload.len()).into());
     
     // Create OP_RETURN script
     let mut builder = bitcoin::script::Builder::new();
@@ -55,6 +62,7 @@ pub async fn mint_ft<W: WalletProvider>(
     }
     
     let script = builder.into_script();
+    console::log_1(&"Created OP_RETURN script".into());
     
     // Create transaction template
     let mut tx = Transaction {
@@ -68,27 +76,42 @@ pub async fn mint_ft<W: WalletProvider>(
             }
         ],
     };
+    console::log_1(&format!("Created transaction template: {:?}", tx).into());
 
     // If mining is required
     if let Some(bitwork) = config.mint_bitworkc.as_ref() {
+        console::log_1(&format!("Mining required with bitwork: {}", bitwork).into());
         let bitwork_info = BitworkInfo::new(bitwork.clone());
         let options = mining_options.unwrap_or_default();
         
         // Mine the transaction
+        console::log_1(&"Starting transaction mining...".into());
         let mining_result = mine_transaction(tx, bitwork_info, options)?;
         tx = mining_result.transaction;
+        console::log_1(&"Mining completed".into());
     }
 
     // Sign the transaction with UTXOs
-    let signed_tx = wallet.sign_transaction(tx, &[]).await
-        .map_err(|e| Error::WalletError(format!("Failed to sign transaction: {}", e)))?;
+    console::log_1(&"Attempting to sign transaction...".into());
+    let signed_tx = match wallet.sign_transaction(tx, &[]).await {
+        Ok(signed) => {
+            console::log_1(&"Transaction signed successfully".into());
+            signed
+        },
+        Err(e) => {
+            console::error_1(&format!("Failed to sign transaction: {}", e).into());
+            return Err(Error::WalletError(format!("Failed to sign transaction: {}", e)));
+        }
+    };
 
     // Get the transaction ID
     let txid = signed_tx.txid().to_string();
+    console::log_1(&format!("Generated transaction ID: {}", txid).into());
 
     // Create token instance
     let mut token = Arc20Token::new(config);
     token.add_holder(address, token.config.mint_amount)?;
+    console::log_1(&"Token instance created and holder added".into());
 
     Ok(AtomicalsTx::new(signed_tx, vec![]).with_atomicals_id(txid))
 }
